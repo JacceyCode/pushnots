a428b4b0bffc97fd6709655ded62c794a2135f12
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-19 10:44:30 +0100

    local notification

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, View } from "react-native";
 import * as Notifications from "expo-notifications";
@@ -5,14 +6,39 @@ import * as Notifications from "expo-notifications";
 Notifications.setNotificationHandler({
   handleNotification: async () => {
     return {
+      shouldShowAlert: true,
       shouldPlaySound: false,
       shouldSetBadge: false,
-      shouldShowAlert: true,
     };
   },
 });
 
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTOFICATION RESPONSE RECEIVED");
+        console.log(response);
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
@@ -21,7 +47,7 @@ export default function App() {
         data: { userName: "Max" },
       },
       trigger: {
-        seconds: 5,
+        seconds: 2,
       },
     });
   }
